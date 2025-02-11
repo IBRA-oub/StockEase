f6c6563cd2269a53df2139f696cc5e75a90830cc
IBRA-oub <brahimbourih@gmail.com>
2025-02-11 22:30:35 +0100

    feat : ajoute pop up pour update de quantity et aussi pour la suppression

--- app/(details)/productDetails.jsx
+++ app/(details)/productDetails.jsx
@@ -1,5 +1,5 @@
-import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import { Alert, Image, Modal, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import AntDesign from '@expo/vector-icons/AntDesign';
 import images from '../../constants/images';
@@ -9,7 +9,38 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { useRouter } from 'expo-router';
 
 const ProductDetails = () => {
+  const [editeQuantity, setEditeQuentity] = useState(false)
+  const [quantity, setquantity] = useState('')
   const router = useRouter()
+
+  // update function
+  const handleSubmit = async (e) => {
+    setEditeQuentity(false)
+    console.log(quantity)
+    e.preventDefault()
+  }
+
+  // delete alert 
+  const handleDelete = () => {
+    Alert.alert(
+      "Confirm Deletion",
+      "Are you sure you want to delete this product?",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        {
+          text: "Confirm",
+          onPress: () => {
+            console.log("Product deleted");
+            
+          }
+        }
+      ]
+    );
+  }
   return (
     <SafeAreaView style={styles.container}>
       <View style={{ width: '100%', height: 44, flexDirection: 'row', alignItems: 'center' }}>
@@ -102,16 +133,44 @@ const ProductDetails = () => {
           </View>
         </View>
 
-        <View style={{ width: '100%', height: 75, flexDirection: 'row', alignItems: 'center', paddingLeft: 10,marginTop:10 }}>
-          <TouchableOpacity style={{ width: '30%', height: '80%', overflow:'hidden',borderRadius:10,justifyContent:'center',alignItems:'center',borderWidth:1,borderColor:'#E3E3E3' }}>
+        <View style={{ width: '100%', height: 75, flexDirection: 'row', alignItems: 'center', paddingLeft: 10, marginTop: 10 }}>
+          <TouchableOpacity onPress={handleDelete} style={{ width: '30%', height: '80%', overflow: 'hidden', borderRadius: 10, justifyContent: 'center', alignItems: 'center', borderWidth: 1, borderColor: '#E3E3E3' }}>
             <FontAwesome name="trash" size={34} color="#C67C4E" />
           </TouchableOpacity>
-          <TouchableOpacity style={{ width: '65%', height: '80%', backgroundColor: '#C67C4E' , overflow:'hidden',borderRadius:10,justifyContent:'center',alignItems:'center' , marginLeft:10 }}>
-            <Text style={{fontSize:27,fontWeight:'bold',color:'white'}}>
-              Edite
+
+          <TouchableOpacity onPress={() => setEditeQuentity(true)} style={{ width: '65%', height: '80%', backgroundColor: '#C67C4E', overflow: 'hidden', borderRadius: 10, justifyContent: 'center', alignItems: 'center', marginLeft: 10 }}>
+            <Text style={{ fontSize: 27, fontWeight: 'bold', color: 'white' }}>
+              Update
             </Text>
           </TouchableOpacity>
         </View>
+
+        {/* update modal */}
+        <Modal visible={editeQuantity} animationType='slide' transparent={true} onRequestClose={''}>
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#1d1d1dd1' }}>
+            <View style={{ width: '80%', backgroundColor: 'white', borderRadius: 10, padding: 20 }}>
+              <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 20, textAlign: 'center' }}>
+                Update Quantity
+              </Text>
+              <View style={{ width: '95%', marginBottom: 20 }}>
+                <Text style={{ fontSize: 16, fontWeight: '600', marginBottom: 5, color: '#C67C4E' }}>Quantity</Text>
+                <TextInput
+                  style={{ height: 50, borderColor: '#C67C4E', borderWidth: 1, borderRadius: 7, paddingLeft: 10, fontSize: 15, }}
+                  placeholder={'22222'}
+                  placeholderTextColor={'gray'}
+                  keyboardType="numeric"
+                  value={quantity}
+                  onChangeText={(e) => setquantity({ ...quantity, quantity: e })}
+                />
+              </View>
+              <TouchableOpacity onPress={handleSubmit} style={{ width: '95%', height: 55, backgroundColor: '#C67C4E', justifyContent: 'center', alignItems: 'center', overflow: 'hidden', borderRadius: 10 }}>
+                <Text style={{ color: 'white', fontSize: 18, fontWeight: '700' }}>
+                  Submit
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
 
       </ScrollView>
     </SafeAreaView>

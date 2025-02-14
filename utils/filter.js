e0a2d2354370261dd026f625715a29b3e8e9197a
IBRA-oub <brahimbourih@gmail.com>
2025-02-14 22:49:38 +0100

    feat : STOC-38 Ajouter une fonctionnalité de tri dynamique des produits.

--- utils/filter.js
+++ utils/filter.js
@@ -1,5 +1,4 @@
-// utils.js
-export const filterAndSortProducts = (products, { selectedCity, filter, searchTerm }) => {
+ export const filterAndSortProducts = (products, { selectedCity, filter, searchTerm }) => {
     const mappedData = products.map(product => {
         if (selectedCity && selectedCity !== 'All'&& product.stocks) {
             const cityStocks = product.stocks.filter(stock => stock?.localisation?.city === selectedCity);
@@ -20,8 +19,10 @@ export const filterAndSortProducts = (products, { selectedCity, filter, searchTe
         filteredData.sort((a, b) => a.name.localeCompare(b.name));
     } else if (filter === 'quantity') {
         filteredData.sort((a, b) => a.stocks.quantity - b.stocks.quantity);
-    } else if (filter === 'price') {
+    } else if (filter === 'priceAsc') {
         filteredData.sort((a, b) => a.price - b.price);
+    } else if (filter === 'priceDesc') {
+        filteredData.sort((a, b) => b.price - a.price);
     }
 
     if (searchTerm) {

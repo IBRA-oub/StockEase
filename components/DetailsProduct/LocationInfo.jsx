76e6a57764bb9fd8a3032b556b2a094e1248ce6e
IBRA-oub <brahimbourih@gmail.com>
2025-02-16 18:32:38 +0100

    feat : STOC-47Intégrer expo-print pour la génération du fichier PDF.

--- components/DetailsProduct/LocationInfo.jsx
+++ components/DetailsProduct/LocationInfo.jsx
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Entypo from '@expo/vector-icons/Entypo';
 
-const LocationInfo = ({productCity,productStockName,productDeltailsName ,productDeltailsType }) => {
+const LocationInfo = ({productCity,productStockName,productDetailsName ,productDetailsType }) => {
     return (
         <>
             <View style={{ width: '100%', height: 45, flexDirection: 'row', alignItems: 'center', paddingLeft: '10' }}>
@@ -10,8 +10,8 @@ const LocationInfo = ({productCity,productStockName,productDeltailsName ,product
                 <Text style={{ fontSize: 16, fontWeight: '600' }}>  {productCity?`${productCity}`:'No stock'} , {productStockName? `${productStockName}`:'For This'}</Text>
             </View>
             <View style={{ width: '100%', height: 35, flexDirection: 'row', alignItems: 'center', paddingLeft: '30' }}>
-                <Text style={{ fontSize: 16, fontWeight: '600' }}>{productDeltailsName} </Text>
-                <Text style={{ fontSize: 16, color: 'gray' }}> ({productDeltailsType} )</Text>
+                <Text style={{ fontSize: 16, fontWeight: '600' }}>{productDetailsName} </Text>
+                <Text style={{ fontSize: 16, color: 'gray' }}> ({productDetailsType} )</Text>
             </View>
         </>
     )
